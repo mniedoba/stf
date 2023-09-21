435a9c1fa7e2413b247fe6622d499d4df6dbece4
Matt Niedoba <matthew.niedoba@inverted.ai>
2023-09-21 12:41:14 -0700

    Add SLURM distributed args.

--- torch_utils/distributed.py
+++ torch_utils/distributed.py
@@ -6,22 +6,28 @@
 # work. If not, see http://creativecommons.org/licenses/by-nc-sa/4.0/
 
 import os
+import re
+import shutil
 import torch
 from . import training_stats
 
 #----------------------------------------------------------------------------
 
 def init():
-    if 'MASTER_ADDR' not in os.environ:
-        os.environ['MASTER_ADDR'] = 'localhost'
-    if 'MASTER_PORT' not in os.environ:
-        os.environ['MASTER_PORT'] = '29500'
-    if 'RANK' not in os.environ:
-        os.environ['RANK'] = '0'
-    if 'LOCAL_RANK' not in os.environ:
-        os.environ['LOCAL_RANK'] = '0'
-    if 'WORLD_SIZE' not in os.environ:
-        os.environ['WORLD_SIZE'] = '1'
+    if is_slurm():
+        distributed_vars = get_slurm_vars()
+    else:
+        distributed_vars = {
+            'MASTER_ADDR': 'localhost',
+            'MASTER_PORT': '29500',
+            'RANK': '0',
+            'LOCAL_RANK': '0',
+            'WORLD_SIZE': '1',
+        }
+
+    for k in distributed_vars:
+        if k not in os.environ:
+            os.environ[k] = distributed_vars[k]
 
     backend = 'gloo' if os.name == 'nt' else 'nccl'
     torch.distributed.init_process_group(backend=backend, init_method='env://')
@@ -29,6 +35,47 @@ def init():
 
     sync_device = torch.device('cuda') if get_world_size() > 1 else None
     training_stats.init_multiprocessing(rank=get_rank(), sync_device=sync_device)
+
+def is_slurm():
+    srun_exists = shutil.which("srun") is not None
+    job_id_exists = os.environ.get("SLURM_JOB_ID") is not None
+    return srun_exists or job_id_exists
+
+def get_slurm_vars():
+    # Master address
+    nodelist = os.environ.get("SLURM_NODELIST", "127.0.0.1")
+    master_addr = resolve_root_node_address(nodelist)
+
+    # Master port:
+    job_id = os.environ.get("SLURM_JOB_ID")
+    if job_id is not None:
+        if job_id is not None:
+            # use the last 4 numbers in the job id as the id
+            default_port = job_id[-4:]
+            # all ports should be in the 10k+ range
+            default_port = int(default_port) + 15000
+        else:
+            default_port = 29500
+
+    global_rank = os.environ["SLURM_PROCID"]
+    local_rank = os.environ["SLURM_LOCALID"]
+    world_size = os.environ["SLURM_NTASKS"]
+
+    return {
+        'MASTER_ADDR': master_addr,
+        'MASTER_PORT': str(default_port),
+        'RANK': global_rank,
+        'LOCAL_RANK': local_rank,
+        'WORLD_SIZE': world_size,
+    }
+
+
+def resolve_root_node_address(nodes):
+    nodes = re.sub(r"\[(.*?)[,-].*\]", "\\1",
+                   nodes)  # Take the first node of every node range
+    nodes = re.sub(r"\[(.*?)\]", "\\1",
+                   nodes)  # handle special case where node range is single number
+    return nodes.split(" ")[0].split(",")[0]
 
 #----------------------------------------------------------------------------
 
